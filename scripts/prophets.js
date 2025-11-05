33f84db2019df098967035bdf5214df76248d883
Kara Lee Carver <karasactualemail@gmail.com>
2025-11-05 09:36:35 -0700

    jason

---
+++ scripts/prophets.js
@@ -0,0 +1,45 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+
+const cards = document.querySelector("#cards");
+
+
+const displayProphets = (prophets) => {
+    prophets.forEach(prophet => {
+
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let birthdate = document.createElement("p");
+        let place = document.createElement("p");
+        let portrait = document.createElement('img');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname} - ${prophet.order}`;
+        birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        place.textContent = `Place of Birth: ${prophet.birthplace}`;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}.`);
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(birthdate);
+        card.appendChild(place);
+        card.appendChild(portrait);
+
+        card.classList.add("card");
+
+        cards.appendChild(card);
+
+        
+    });
+}
+
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+getProphetData();
